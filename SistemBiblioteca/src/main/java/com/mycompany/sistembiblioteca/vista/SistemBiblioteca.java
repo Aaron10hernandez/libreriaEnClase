e6b27913a4675c3a69ad5f03cf303fdd2b69ddde
Alejandro Herrera <alejandroherrerab2007@gmail.com>
2026-04-23 18:53:05 -0500

    Update SistemBiblioteca.java

--- SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/vista/SistemBiblioteca.java
+++ SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/vista/SistemBiblioteca.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.sistembiblioteca;
+package com.mycompany.sistembiblioteca.vista;
 
 import java.util.Scanner;
 

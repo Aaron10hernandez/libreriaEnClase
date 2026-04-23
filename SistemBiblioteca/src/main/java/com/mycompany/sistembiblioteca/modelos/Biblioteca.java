db16ed8cde4bffd8bfc40854bdbc9569046ffd39
PuntoAtoon <aarondh.10@gmail.com>
2026-04-23 18:18:39 -0500

    Aaron1
    cree modelos

---
+++ SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Biblioteca.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistembiblioteca.modelos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Biblioteca {
+    
+    
+    
+}
---
+++ SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Prestamo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistembiblioteca.modelos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prestamo {
+    
+}

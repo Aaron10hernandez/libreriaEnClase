2bddf41a414ea1e5f6636a00d8e0768f3f6dd78d
jcassianih1 <jcassianih1@unicartagena.edu.co>
2026-04-23 18:53:14 -0500

    cassio

--- SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Categoria.java
+++ SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Categoria.java
@@ -4,10 +4,37 @@
  */
 package com.mycompany.sistembiblioteca.modelos;
 
+import com.mycompany.sistembiblioteca.Modelos.Libro;
+import java.util.ArrayList;
+
 /**
  *
  * @author USUARIO
  */
 public class Categoria {
+    private static int contadorId = 1;
+    private int Id;
+    private final String name;
+    private ArrayList<Libro> Libros;
+
+    public Categoria(String name) {
+        this.Id = contadorId++;
+        this.name = name;
+        this.Libros = new ArrayList<>();
+    }
+    
+    public void agregarLibro(Libro libro) {
+        Libros.add(libro);
+    }
+
+    public int getId() { return Id; }
+    public String getNombre() { return name; }
+    public ArrayList<Libro> getLibros() { return Libros; }
+
+    @Override
+    public String toString() {
+        return "ID: " + Id + " | Categoría: " + name;
+    }
+    
     
 }

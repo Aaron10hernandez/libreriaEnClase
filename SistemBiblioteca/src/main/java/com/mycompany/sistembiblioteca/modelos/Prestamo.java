c3067ad7004ec7351b3e3770295d7b5f5d159db1
jcassianih1 <jcassianih1@unicartagena.edu.co>
2026-04-23 18:59:54 -0500

    casssio

--- SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Prestamo.java
+++ SistemBiblioteca/src/main/java/com/mycompany/sistembiblioteca/modelos/Prestamo.java
@@ -10,4 +10,24 @@ package com.mycompany.sistembiblioteca.modelos;
  */
 public class Prestamo {
     
+    private static int contadorId = 1;
+    private final int id;
+    private Libro libro;
+    private String usuario;
+    private String fecha;
+
+    public Prestamo(Libro libro, String usuario, String fecha) {
+        this.id = contadorId++;
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fecha = fecha;
+        this.libro.setDisponible(false);
+    }
+
+    public int getId() { return id; }
+    public Libro getLibro() { return libro; }
+    
+    public void devolver() {
+        this.libro.setDisponible(true);
+    }
 }
